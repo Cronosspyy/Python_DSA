1d14095575e245330c187ac6a4692f3cc59fa529
Cronosspyy <aaryan04choudhary@gmail.com>
2026-08-28 00:53:45 +0530

    updation

---
+++ selection-sort.py
@@ -0,0 +1,11 @@
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(0,n):
+        min_index = i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_index]:
+                min_index = j
+        nums[i],nums[min_index] = nums[min_index],nums[i]
+    return nums
+
+print(selection_sort(nums = [4,7,3,5,8,9,1,2,6]))
